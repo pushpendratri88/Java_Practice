be5370b700e440e9d2a5cbbf5f6f75ce0e788f4b
Pushpendra Tripathi <pushpendra.tri88@gmail..com>
2024-07-02 00:13:36 +0530

    Java 8 Stream api Practice

--- src/com/brainstrom/Main.java
+++ src/com/brainstrom/Main.java
@@ -3,6 +3,15 @@ package com.brainstrom;
 public class Main {
     public static void main(String[] args) {
     Utilities utilities = new Utilities();
-    utilities.findNoStartWith1();
+
+//        utilities.findNoStartWith1();
+//        utilities.findEvenNo();
+//        utilities.findDuplicateElement();
+//        utilities.findDuplicateUniqueElement();
+//        utilities.findFirstElement();
+//        utilities.totalCount();
+//        utilities.findMax();
+//        utilities.sortInt();
+        utilities.DescSortInt();
     }
 }
--- src/com/brainstrom/Utilities.java
+++ src/com/brainstrom/Utilities.java
@@ -1,6 +1,9 @@
 package com.brainstrom;
 
-import java.util.Arrays;
+
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class Utilities {
 
@@ -8,6 +11,60 @@ public class Utilities {
         int[] intArray = new int[] {1,3,5,5,7,7,7,9,11,11,15,15,17,17,17,19,21,21,21,21};
         Arrays.stream(intArray)
                 .filter(num -> Integer.toString(num).startsWith("1")).forEach(value -> System.out.println(value));
+    }
+//    10,15,8,49,25,98,32
+    public void findEvenNo (){
+        List<Integer>  mixedInteger = Arrays.asList(10,15,8,49,25,98,32);
+        mixedInteger.stream().filter(n -> n%2 == 0).forEach(val -> System.out.println(val));
+    }
+//  1,3,5,5,7,7,7,9,11,11,15,15,17,17,17,19,21,21,21,21
+    public void findDuplicateElement(){
+        List<Integer> duplicateInt = Arrays.asList(1,3,5,5,7,7,7,9,11,11,15,15,17,17,17,19,21,21,21,21);
+        Set<Integer> uniqueInt = new HashSet<>();
+        duplicateInt.stream().filter(num -> !uniqueInt.add(num)).forEach(System.out::println);
+    }
+
+//  1,3,5,5,7,7,7,9,11,11,15,15,17,17,17,19,21,21,21,21
+    public void findDuplicateUniqueElement(){
+        List<Integer> duplicateInt = Arrays.asList(1,3,5,5,7,7,7,9,11,11,15,15,17,17,17,19,21,21,21,21);
+        Set<Integer> uniqueInt = new HashSet<>();
+        Set<Integer> duplicates =  duplicateInt.stream().filter(num -> !uniqueInt.add(num)).collect(Collectors.toSet());
+        duplicates.forEach(System.out::println);
+    }
+
+//  1,3,5,5,7,7,7,9,11,11,15,15,17,17,17,19,21,21,21,21
+    public  void findFirstElement(){
+        int[] intarray = new int[]{ 15,15,17,17,17,19,21,21,21,21};
+        OptionalInt optionalFirst = Arrays.stream(intarray).findFirst();
+        if(optionalFirst.isPresent()){
+            System.out.println(optionalFirst.getAsInt());
+        }
+    }
+
+//  1,3,5,5,7,7,7,9,11,11,15,15,17,17,17,19,21,21,21,21
+    public void totalCount(){
+        int[] intArray = new int[] {1,3,5,5,7,7,7,9,11,11,15,15,17,17,17,19,21,21,21,21};
+        long count = Arrays.stream(intArray).count();
+        System.out.println(count);
+    }
+
+//  10,15,8,49,25,98,98,32,15
+    public void findMax(){
+        List<Integer> listOfArray = Arrays.asList(10,15,8,49,25,98,98,32,15);
+        int max = listOfArray.stream().max(Integer::compare).get();
+        System.out.println(max);
+    }
+//    10,15,8,49,25,98,98,32,15
+    public void sortInt(){
+        int[] unSortInt = new int[] {10,15,8,49,25,98,98,32,15};
+        IntStream sortedInt = Arrays.stream(unSortInt).sorted();
+        sortedInt.forEach(val -> System.out.println(val));
+    }
+
+    public void DescSortInt(){
+        int[] unSortInt = new int[] {10,15,8,49,25,98,98,32,15};
+        IntStream sortedInt = Arrays.stream(unSortInt).boxed().sorted(Comparator.reverseOrder()).mapToInt(Integer:: intValue);
+        sortedInt.forEach(val -> System.out.println(val));
     }
 
 }
