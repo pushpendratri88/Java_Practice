d74d529918e9e6646f4e56160a4568d3d06a2230
Pushpendra.tripathi <pushpendra.tri88@gmail.com>
2024-08-23 12:49:21 +0530

    Leetcode

---
+++ src/com/brainstrom/leetCode/GasStation.java
@@ -0,0 +1,13 @@
+package com.brainstrom.leetCode;
+
+public class GasStation {
+    public static void main(String[] args) {
+        int[] gas = new int[] {5,1,2,3,4};
+        int[] cost = new int[]{4,4,1,5,1};
+        System.out.println(canCompleteCircuit(gas,cost));
+
+    }
+    public static int canCompleteCircuit(int[] gas, int[] cost) {
+
+    }
+}
---
+++ src/com/brainstrom/mathsProblem/DecimalValueUpto6.java
@@ -0,0 +1,31 @@
+package com.brainstrom.mathsProblem;
+
+import java.text.DecimalFormat;
+import java.util.Arrays;
+import java.util.Formatter;
+import java.util.List;
+
+public class DecimalValueUpto6 {
+    public static void main(String[] args) {
+        List<Integer> arr = Arrays.asList(1,1,0,-1,-1);
+        double positiveNumberCount = 0;
+        double nagativeNumberCount = 0;
+        double zeroCount = 0;
+        double listSize =   Double.valueOf(arr.size());
+        for(Integer num : arr){
+            if(num < 0){
+                nagativeNumberCount++;
+            }
+            else if(num > 0){
+                positiveNumberCount++;
+            }
+            else{
+                zeroCount++;
+            }
+        }
+        DecimalFormat fm = new DecimalFormat("#.######");
+        System.out.println(String.format("%.6f", positiveNumberCount/listSize));
+        System.out.println(String.format("%.6f",nagativeNumberCount/listSize));
+        System.out.println(String.format("%.6f",zeroCount/listSize));
+    }
+}
