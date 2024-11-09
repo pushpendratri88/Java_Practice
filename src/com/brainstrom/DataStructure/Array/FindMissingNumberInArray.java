ad1ca3aeb7ba707de7785d5b2b9b16e0f82e78f8
Pushpendra Tripathi <pushpendra.tri88@gmail..com>
2024-11-09 18:45:55 +0530

    Array Practice

---
+++ src/com/brainstrom/DataStructure/Array/FindMissingNumberInArray.java
@@ -0,0 +1,19 @@
+package com.brainstrom.DataStructure.Array;
+
+public class FindMissingNumberInArray {
+    public static void main(String[] args) {
+        int[] myArray= {1,2,3,4,6};
+        System.out.println(findMissingNumberInArray(myArray));
+    }
+
+    static int findMissingNumberInArray(int[] arr) {
+        int n = arr.length + 1;
+        int expectedSum = (n * (n+1))/2;
+        int acctualSum =  0;
+
+        for(int i=0; i< arr.length; i++){
+            acctualSum =acctualSum + arr[i];
+        }
+        return expectedSum - acctualSum;
+    }
+}
